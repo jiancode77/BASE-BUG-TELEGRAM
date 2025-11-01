bf4097ccd9703f70f681ca5d2219d27a0828ef69
Jian Unknown <bintangkecilku3374@gmail.com>
2025-11-01 18:30:34 +0700

    Create Known-Base.js

---
+++ Known-Base.js
@@ -0,0 +1,26 @@
+// Known-Base.js
+module.exports = {
+    // Konfigurasi database atau penyimpanan known numbers
+    knownNumbers: [],
+    
+    addKnownNumber: function(number) {
+        if (!this.knownNumbers.includes(number)) {
+            this.knownNumbers.push(number);
+            return true;
+        }
+        return false;
+    },
+    
+    removeKnownNumber: function(number) {
+        const index = this.knownNumbers.indexOf(number);
+        if (index > -1) {
+            this.knownNumbers.splice(index, 1);
+            return true;
+        }
+        return false;
+    },
+    
+    getKnownNumbers: function() {
+        return this.knownNumbers;
+    }
+};
