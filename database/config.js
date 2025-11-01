1d65dd78cb7d5d371390055c4e7a54a0bd122ba3
Jian Unknown <bintangkecilku3374@gmail.com>
2025-11-01 19:06:32 +0700

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,4 +1,5 @@
 // database/config.js
 module.exports = {
-    BOT_TOKEN: '7987531387:AAE2xVu_asKwVZvsBVP9hHuctPTaQHjmuTM'
+    BOT_TOKEN: '7987531387:AAE2xVu_asKwVZvsBVP9hHuctPTaQHjmuTM',
+    OWNER_ID: '7808981311'
 };
